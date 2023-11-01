f6b22b3200c459f1bd58195460c550321d4f6235
Rosenya <lajluch@gmail.com>
2023-11-01 20:49:32 +0100

    Kurs Udemy: "Java - ambitny start" Zadanie 2: Query parametry - pierwszy sposób na "content negotiation". Rozwiązanie

--- src/main/java/ex2/QuerryParameters.java
+++ src/main/java/ex2/QuerryParameters.java
@@ -1,6 +1,5 @@
 package ex2;
 
-import ex1.LogToConsoleSLF4J;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet
+@WebServlet(name = "Ex2", urlPatterns = {"api/*"})
 public class QuerryParameters extends HttpServlet {
 //Query parametry - pierwszy sposób na "content negotiation".
 //    1. Przeczytać dokumentację ServletRequest (baza dla HttpServletRequest)
@@ -25,8 +24,12 @@ public class QuerryParameters extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String name = req.getParameter("Mistrz");
+        if (name != null) {
+            LOGGER.info(name);
+        } else{
+            LOGGER.info("nie podano parametru");
+        }
     }
-
 
 }
