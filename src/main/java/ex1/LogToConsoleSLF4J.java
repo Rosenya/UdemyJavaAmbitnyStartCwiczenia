449ae5aa1b8119e35fd7995511369e1a07506daa
Rosenya <lajluch@gmail.com>
2023-11-01 19:04:53 +0100

    Kurs Udemy: "Java - ambitny start" Zadanie 1: Logowanie przez konsolę SLF4J -zastąpienie System.out.println() przez Logger

--- src/main/java/ex1/LogToConsoleSLF4J.java
+++ src/main/java/ex1/LogToConsoleSLF4J.java
@@ -4,7 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class LogToConsoleSLF4J {
-
+//Zadanie 1: Logowanie przez konsolę SLF4J
+//-zastąpienie System.out.println() przez Logger
     public static void main(String[] args) {
         Logger LOGGER = LoggerFactory.getLogger(LogToConsoleSLF4J.class);
 
